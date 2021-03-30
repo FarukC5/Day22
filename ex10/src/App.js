fe4ff2f203a3da739cd3af7d4e73b9a32e9c6c10
FarukC5 <faruk_cajic@yahoo.com>
2021-03-30 13:01:16 +0200

    Create Props within Simple Component

---
+++ ex10/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import Box from "./Box";
+import "./App.css";
+
+function App() {
+  // sa div ili bez?
+  return (
+    <div className="App">
+      <Box size="20" />;
+    </div>
+  );
+}
+
+export default App;
---
+++ ex10/src/Box.js
@@ -0,0 +1,10 @@
+// example simple component
+import React, { Component } from "react";
+//import React from "react";
+
+function Box(props) {
+ // only change code below this line
+  return <h2>This is a {props.size}px Box!</h2>
+} // only chane code above this line
+
+export default Box;
