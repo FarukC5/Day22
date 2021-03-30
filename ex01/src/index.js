c543c9f7206a2c8f13fc8d383011099811ddbb83
FarukC5 <faruk_cajic@yahoo.com>
2021-03-30 10:56:51 +0200

    Components

---
+++ ex01/src/index.js
@@ -0,0 +1,11 @@
+// src/index.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"; // This line added
+import "./index.css";
+
+// Remove code below this line
+
+// Remove code below this line
+ReactDOM.render(<App />, document.getElementById("root"));
