c2867faa553edfa262c9bfd06f50e514f9a3439b
FarukC5 <faruk_cajic@yahoo.com>
2021-03-30 12:32:11 +0200

    Create a Simple BOx Component

---
+++ ex07/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import Box from "./Box";
+import "./App.css";
+
+function App() {
+  return (
+    // dal treba div ili ne
+    <div className="App">
+      <Box />
+    </div>
+  );
+}
+
+export default App;
